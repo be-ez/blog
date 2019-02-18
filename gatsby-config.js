b14189cc4350bff667c7cc6ae2f3c7f5288c10bd
be-ez <a@xn--4qe.com>
2019-02-17 20:38:50 -0800

    Added gatsby-plugin-offline to enable service_worker.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
     'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
