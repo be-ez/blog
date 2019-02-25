06b070c44e6b67c29433fb0957535ca6c3b6211b
Alex Shulman <alex.shulman@ouster.io>
2019-02-25 01:32:56 -0800

    Added Card view

--- src/components/contentful/Card.js
+++ src/components/contentful/Card.js
@@ -1,13 +1,38 @@
 import React from "react"
 import { graphql } from "gatsby"
 import ContentfulContent from './GenericContent'
+import Img from 'gatsby-image'
+import {
+  Box,
+  Card,
+  Heading,
+  Text
+} from 'rebass'
 
 export default function ContentfulCard({content, slug}) {
-  console.log(content)
   const inner_content =content.content
   return (
     <div>
-      <ContentfulContent slug={slug} content={inner_content} />
+      <Box width={1/3}>
+      <Card
+
+        boxShadow='0 0 16px rgba(0, 0, 0, .25)'>
+        <Img
+            alt={content.slug}
+            fluid={content.image.fluid} />
+        <Box p={1} px={2}>
+          <Heading as='h1'>
+            {inner_content.title}
+          </Heading>
+          <Text
+          dangerouslySetInnerHTML={{
+            __html: inner_content.body.childContentfulRichText.html,
+          }}
+          fontSize={3}>
+          </Text>
+        </Box>
+      </Card>
+      </Box>
     </div>
   )
 }
@@ -15,6 +40,11 @@ export default function ContentfulCard({content, slug}) {
 export const cardFragment = graphql`
   fragment ContentfulCardFragment on ContentfulCard {
     slug
+    image {
+      fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+        ...GatsbyContentfulFluid_tracedSVG
+      }
+    }
     content {
       ... on ContentfulGenericContent {
         ...GenericContentFragment
