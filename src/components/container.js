8097a6a38ef26c08aae6490655adf76b6972b47b
be-ez <ajshulman@gmail.com>
2019-02-26 07:35:11 -0800

    Trying to get fullscreen on iphone x(s) (max)

--- src/components/container.js
+++ src/components/container.js
@@ -3,9 +3,6 @@ import Helmet from 'react-helmet'
 
 export default ({ children }) => (
   <div style={{ maxWidth: 1180, margin: '0 auto' }}>
-  <Helmet name="apple-mobile-web-app-capable" content="yes" />
-  <Helmet name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-  <meta ></meta>
     {children}
   </div>
 )
